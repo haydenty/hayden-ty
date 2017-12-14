df75ec9980f8011bb7e17aa428caf53b5fc27da9
haydenty <htwade51@gmail.com>
2017-12-13 19:46:43 -0600

    point to prod api

--- src/assets/app.constants.js
+++ src/assets/app.constants.js
@@ -1,4 +1,5 @@
 const appConstants = {
     appName:'haydentylocal',
-    crudApi: 'http://localhost:3002/api/'
+    //crudApi: 'http://localhost:3002/api/'
+    crudApi:'http://haydenty.com/api/'
 };
